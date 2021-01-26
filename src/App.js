59c3b9db8c9f7cb8f5bdc6bd4c382fe053dab343
Noah Iniguez <niniguezg@gmail.com>
2021-01-26 10:01:52 -0600

    Initialize codebase

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react'
+
+import ExampleViz from './ExampleViz';
+
+const App = () => {
+	const [wells, setWells] = useState([])
+
+	const loadWells = () => {
+		fetch("http://localhost:3000/wells").then(res => res.json()).then(
+			(wells) => {
+				setWells(wells);
+			}, 
+			(error) => {
+				console.log('loadWells: ', error);
+			}
+		);
+	}
+
+	useEffect(() => {
+		loadWells();
+		console.log(wells);
+	}, []);
+
+	return (
+		<div 
+			className="App" 
+			style={{
+				height: "100vh",
+				position: "relative",
+				padding: "16px",
+				"box-sizing": "border-box",
+				"background-color": "#000"
+			}}
+		>
+			<ExampleViz wells={wells}/>
+		</div>
+	);
+}
+
+export default App;
