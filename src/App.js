b96a4588fd7ce2efe42643dfc5ded9bba2dae77e
Noah Iniguez <niniguez@illinoisrocstar.com>
2021-01-26 15:48:57 -0600

    Add polyline implementation

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-import ExampleViz from './ExampleViz';
+import ExampleViz from "./ExampleViz";
 
 const App = () => {
-	const [wells, setWells] = useState([])
+  const [wells, setWells] = useState([]);
 
-	const loadWells = () => {
-		fetch("http://localhost:3000/wells").then(res => res.json()).then(
-			(wells) => {
-				setWells(wells);
-			}, 
-			(error) => {
-				console.log('loadWells: ', error);
-			}
-		);
-	}
+  const loadWells = () => {
+    fetch("http://localhost:3000/wells")
+      .then((res) => res.json())
+      .then(
+        (wells) => {
+          setWells(wells);
+        },
+        (error) => {
+          console.log("loadWells: ", error);
+        }
+      );
+  };
 
-	useEffect(() => {
-		loadWells();
-		console.log(wells);
-	}, []);
+  useEffect(() => {
+    loadWells();
+    // console.log(wells);
+  }, []);
 
-	return (
-		<div 
-			className="App" 
-			style={{
-				height: "100vh",
-				position: "relative",
-				padding: "16px",
-				"box-sizing": "border-box",
-				"background-color": "#000"
-			}}
-		>
-			<ExampleViz wells={wells}/>
-		</div>
-	);
-}
+  return (
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+        position: "relative",
+        padding: "16px",
+        "box-sizing": "border-box",
+        "background-color": "#000",
+      }}
+    >
+      <ExampleViz wells={wells} />
+    </div>
+  );
+};
 
 export default App;
--- src/ExampleViz.js
+++ src/ExampleViz.js
@@ -1,8 +1,46 @@
-import * as React from 'react';
-import { Canvas } from 'react-three-fiber';
-import Controls from './Controls';
+import * as React from "react";
+import { Canvas } from "react-three-fiber";
+import Controls from "./Controls";
+import * as THREE from "three";
 
 const ExampleViz = ({}) => {
+  const polyline = {
+    points: [
+      [0, 0, 1],
+      [0, 0, 0.5],
+      [0, 0, 0],
+      [0, 0, -0.5],
+      [0, 0, -1],
+    ],
+    colors: ["#FFFFFF", "#FF0000", "#00FF00", "#0000FF"],
+  };
+
+  const geometry = new THREE.BufferGeometry();
+
+  const points = [];
+  let i;
+  for (i = 0; i < polyline.points.length; i++) {
+    let point = polyline.points[i];
+    points.push(new THREE.Vector3(point[0], point[1], point[2]));
+  }
+  const materials = [];
+  for (i = 0; i < polyline.colors.length; i++) {
+    let color = polyline.colors[i];
+    let material = new THREE.LineBasicMaterial({
+      color: color,
+      linewidth: 10,
+      linecap: "round",
+      linejoin: "round",
+    });
+    materials.push(material);
+  }
+
+  const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+
+  for (i = 0; i < polyline.points.length - 1; i++) {
+    lineGeometry.addGroup(i, 2, i);
+  }
+
   return (
     <Canvas camera={{ position: [0, 0, 5] }}>
       <Controls />
@@ -13,10 +51,11 @@ const ExampleViz = ({}) => {
         groundColor="#080820"
         intensity={1.0}
       />
-      <mesh position={[0, 0, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
-        <cylinderBufferGeometry attach="geometry" args={[0.5, 0.5, 0.15, 32]} />
-        <meshStandardMaterial attach="material" color="#fff" />
-      </mesh>
+      <line
+        position={[0, -2.5, -10]}
+        geometry={lineGeometry}
+        material={materials}
+      />
     </Canvas>
   );
 };
