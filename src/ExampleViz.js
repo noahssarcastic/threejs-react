92f4bd482d10cdbe736e67767bf64e00f03ec3e0
Noah Iniguez <niniguezg@gmail.com>
2021-01-27 00:15:55 -0600

    Finish polyline component

--- src/ExampleViz.js
+++ src/ExampleViz.js
@@ -1,9 +1,9 @@
-import * as React from "react";
 import { Canvas } from "react-three-fiber";
-import Controls from "./Controls";
-import * as THREE from "three";
 
-const ExampleViz = ({}) => {
+import Controls from "./Controls";
+import Polyline from './Polyline'
+
+const ExampleViz = () => {
   const polyline = {
     points: [
       [0, 0, 1],
@@ -15,32 +15,6 @@ const ExampleViz = ({}) => {
     colors: ["#FFFFFF", "#FF0000", "#00FF00", "#0000FF"],
   };
 
-  const geometry = new THREE.BufferGeometry();
-
-  const points = [];
-  let i;
-  for (i = 0; i < polyline.points.length; i++) {
-    let point = polyline.points[i];
-    points.push(new THREE.Vector3(point[0], point[1], point[2]));
-  }
-  const materials = [];
-  for (i = 0; i < polyline.colors.length; i++) {
-    let color = polyline.colors[i];
-    let material = new THREE.LineBasicMaterial({
-      color: color,
-      linewidth: 10,
-      linecap: "round",
-      linejoin: "round",
-    });
-    materials.push(material);
-  }
-
-  const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
-
-  for (i = 0; i < polyline.points.length - 1; i++) {
-    lineGeometry.addGroup(i, 2, i);
-  }
-
   return (
     <Canvas camera={{ position: [0, 0, 5] }}>
       <Controls />
@@ -51,11 +25,7 @@ const ExampleViz = ({}) => {
         groundColor="#080820"
         intensity={1.0}
       />
-      <line
-        position={[0, -2.5, -10]}
-        geometry={lineGeometry}
-        material={materials}
-      />
+      <Polyline points={polyline.points} colors={polyline.colors}/>
     </Canvas>
   );
 };
---
+++ src/Polyline.js
@@ -0,0 +1,51 @@
+import { BufferGeometry, Vector3, LineBasicMaterial } from "three";
+import { useEffect, useState } from "react";
+
+const Polyline = (props) => {
+	const { points, colors } = props;
+
+	const [materials, setMaterials] = useState();
+	const [geometry, setGeometry] = useState();
+
+	const drawSegments = () => {
+		const vectors = [];
+		let i;
+		for (i = 0; i < points.length; i++) {
+			let point = points[i];
+			vectors.push(new Vector3(point[0], point[1], point[2]));
+		}
+		const materials = [];
+		for (i = 0; i < colors.length; i++) {
+			let color = colors[i];
+			let material = new LineBasicMaterial({
+				color: color,
+				linewidth: 10,
+				linecap: "round",
+				linejoin: "round",
+			});
+			materials.push(material);
+		}
+
+		const lineGeometry = new BufferGeometry().setFromPoints(vectors);
+		for (i = 0; i < points.length - 1; i++) {
+			lineGeometry.addGroup(i, 2, i);
+		}
+
+		setGeometry(lineGeometry);
+		setMaterials(materials);
+	};
+
+	useEffect(() => {
+		drawSegments();
+	}, [points, colors]);
+
+	return (
+		<line
+			position={[0, -2.5, -10]}
+			geometry={geometry}
+			material={materials}
+		/>
+	);
+};
+
+export default Polyline;
